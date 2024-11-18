e9aafbe8886fb1b08622d60d174f3122cedc52c4
Sean Collins <Sean.Collins01@student.csulb.edu>
2024-11-18 13:20:02 -0800

    formatting container

--- src/components/homepage_layout.js
+++ src/components/homepage_layout.js
@@ -1,20 +1,17 @@
 import * as React from 'react'
-//import {Link} from 'gatsby'
+import {Link} from 'gatsby'
 import {container} from './layout.module.css'
+import { StaticImage } from 'gatsby-plugin-image'
 
-const Layout = ({pageBody}) => {
+const Layout = ({children}) => {
     return (
         <div className={container}>
-            <h1>Sean Collins</h1>
             <nav>
-                <ul>
-                    <li><a href="https://www.linkedin.com/in/sean-collins-4a6279252/" target="_blank">LinkedIn</a></li>
-                    <li><a href="https://github.com/Drink-some-water" target="_blank">GitHub</a></li>
-                    <li><a href="mailto:sean.collins.developer@gmail.com" target="_blank">Contact Me</a></li>
-                </ul>
+                <Link to="../pages/index">Home</Link>
+                <Link to="../pages/works">Porfolio</Link>
             </nav>
             <main>
-                {pageBody}
+                {children}
             </main>
         </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,27 @@
 import * as React from 'react'
 import Layout from '../components/homepage_layout'
+import {imageCircle} from '../components/layout.module.css'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-
+      <h2>Sean Collins' Portfolio</h2>
+      <h3>Thanks for visiting!</h3>
+      <nav>
+        <p>resume</p>
+        <a href="https://www.linkedin.com/in/sean-collins-4a6279252/" target="_blank">LinkedIn</a>
+        <a href="https://github.com/Drink-some-water" target="_blank">GitHub</a>
+        <a href="mailto:sean.collins.developer@gmail.com" target="_blank">Contact Me</a>
+      </nav>
+      <StaticImage
+        alt="A picture of Sean Collins looking professional at home"
+        src="../images/portrait.jpg"
+        className={imageCircle}
+      />
+      <p>Placeholder content</p>
     </Layout>
   )
 }
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Sean's Porfolio</title>
 export default IndexPage
